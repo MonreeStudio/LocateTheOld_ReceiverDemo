d5607263a4bed1c74e13a42c8ec451f931b103dd
MonreeStudio <monreestudio@outlook.com>
2020-02-24 23:58:57 +0800

    引入了百度地图的定位API，实现了经纬度获取功能。

--- app/src/main/java/com/monree/receiverdemo/MainActivity.java
+++ app/src/main/java/com/monree/receiverdemo/MainActivity.java
@@ -11,6 +11,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.graphics.RenderNode;
+import android.location.Location;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
@@ -19,37 +21,95 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button getLocationBtn;
     TextView locationTv;
     IntentFilter filter;
     SmsReceiver receiver;
+    String locationInfo;
+    public LocationClient mLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(new MyLocationListener());
+
         setContentView(R.layout.activity_main);
         getLocationBtn = findViewById(R.id.GetLocationButton);
         locationTv = findViewById(R.id.LocationTextView);
+        locationInfo = "";
         filter = new IntentFilter();
         filter.addAction("android.provider.Telephony.SMS_RECEIVED");
         receiver = new SmsReceiver();
         registerReceiver(receiver,filter);
+
+        List<String> permissionList = new ArrayList<>();
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                !=PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.READ_PHONE_STATE)
+                !=PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                !=PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED){
+            //ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS},1);
+            permissionList.add(Manifest.permission.SEND_SMS);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECEIVE_SMS)
+                != PackageManager.PERMISSION_GRANTED){
+            //ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.RECEIVE_SMS},2);
+            permissionList.add(Manifest.permission.RECEIVE_SMS);
+        }
+        if(!permissionList.isEmpty()){
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this,permissions,1);
+        }
+        else{
+
+        }
+
         getLocationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS)
-                        != PackageManager.PERMISSION_GRANTED){
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS},1);
-                }
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECEIVE_SMS)
-                        != PackageManager.PERMISSION_GRANTED){
-                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.RECEIVE_SMS},2);
-                }
+                requestLocation();
             }
         });
+    }
+
+    private void requestLocation(){
+        //initLocation();
+        if(mLocationClient.isStarted())
+            mLocationClient.stop();
+        mLocationClient.start();
+    }
+
+    private void initLocation(){
+        LocationClientOption option = new LocationClientOption();
+        option.setScanSpan(5000);
+        mLocationClient.setLocOption(option);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocationClient.stop();
     }
 
     public class SmsReceiver extends BroadcastReceiver{
@@ -70,19 +130,44 @@ public class MainActivity extends AppCompatActivity {
             }
             if(content.toString().contains("1908")){
                 try{
-                    Toast.makeText(MainActivity.this,"回复成功",Toast.LENGTH_SHORT).show();
+                    requestLocation();
+                    Thread.sleep(5000);
                     sendSMSS(sender);
+                    Toast.makeText(MainActivity.this,"回复成功",Toast.LENGTH_SHORT).show();
                 }
                 catch (Exception e){
                     e.printStackTrace();
-                    Toast.makeText(MainActivity.this,"权限不足",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this,"回复失败",Toast.LENGTH_SHORT).show();
                 }
             }
         }
     }
 
+    public class MyLocationListener implements BDLocationListener{
+
+        @Override
+        public void onReceiveLocation(final BDLocation location) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    StringBuilder currentPosition = new StringBuilder();
+                    currentPosition.append("纬度：").append(location.getLatitude()).append("\n");
+                    currentPosition.append("经度：").append(location.getLongitude()).append("\n");
+                    currentPosition.append("定位方式：");
+                    if(location.getLocType()==BDLocation.TypeGpsLocation){
+                        currentPosition.append("GPS");
+                    }else if(location.getLocType() == BDLocation.TypeNetWorkLocation){
+                        currentPosition.append("网络");
+                    }
+                    locationInfo = currentPosition.toString();
+                    locationTv.setText(currentPosition);
+                }
+            });
+        }
+    }
+
     private void sendSMSS(String phoneNumber) {
-        String content = locationTv.getText().toString().trim();
+        String content = locationInfo;
         String phone = phoneNumber;
         if (!content.isEmpty()&&!phone.isEmpty()) {
             SmsManager manager = SmsManager.getDefault();
@@ -102,17 +187,19 @@ public class MainActivity extends AppCompatActivity {
         //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode){
             case 1:
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-
+                if(grantResults.length > 0){
+                    for(int result : grantResults){
+                        if(result != PackageManager.PERMISSION_GRANTED){
+                            Toast.makeText(this,"必须同意所有权限才能使用本程序",Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    requestLocation();
                 }
                 else {
-                    Toast.makeText(this,"你取消了授权",Toast.LENGTH_SHORT).show();
-                }
-            case 2:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //这里写操作 如send（）； send函数中New SendMsg （号码，内容）；
-                } else {
-                    Toast.makeText(this, "你取消了授权", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this,"发生未知错误",Toast.LENGTH_SHORT).show();
+                    finish();
                 }
                 break;
             default:
